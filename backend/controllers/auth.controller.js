cbc61363e043aa870b4061a0e4d6c3f3bff3384d
Sansala Gangadari <slicchandrika@gmail.com>
2025-03-30 02:49:43 +0530

    EndPoints for Movies

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -68,7 +68,37 @@ export async function Signup(req, res) {
 }
 
 export async function Login(req, res) {
-  res.send("Login route");
+  try {
+      const {email,password} = req.body;
+      if (!email || !password){
+        return res.status(400).json({success:false,message: "All fields are required"});
+      }
+
+      const user = await User.findOne({email:email});
+
+      if (!user){
+        return res.status(400).json({success:false,message: "Invalid credentials"});
+      }
+
+      const isPasswordCorrect = await bcrypt.compare(password,user.password);
+
+      if(!isPasswordCorrect){
+        return res.status(400).json({success:false,message: "Invalid credentials"});
+      }
+
+      generateTokenAndSetCookie(user._id,res);
+      res.status(200).json({
+        success:true,
+        user:{
+          ...user._doc,
+          password: ""
+        }
+      })
+
+  } catch (error) {
+      console.log("Error in login controller" , error.message);
+      res.status(500).json({success:false,message:"Internal server error"});
+  }
 }
 
 export async function Logout(req, res) {
---
+++ backend/routes/movie.route.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { getTrendingMovies } from "../controllers/movie.controller.js";
+import { getMovieTrailers } from "../controllers/movie.controller.js";
+import {getMovieDetails} from "../controllers/movie.controller.js";
+import {getSimilarMovies} from "../controllers/movie.controller.js";
+import {getMoviesByCategory} from "../controllers/movie.controller.js";
+
+const router = express.Router();
+
+router.get("/trending" , getTrendingMovies);
+router.get("/:id/trailers" , getMovieTrailers);
+router.get("/:id/details" , getMovieDetails);
+router.get("/:id/similar" , getSimilarMovies);
+router.get("/:category" , getMoviesByCategory);
+
+
+
+
+export default router;
