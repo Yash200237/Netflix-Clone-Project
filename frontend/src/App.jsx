beae67640ba5c2a8bfd4b43d580df4cad9615133
Dulakshi <dulakshiabeynayake@gmail.com>
2025-04-05 11:32:42 +0530

    Login , SignUp, Home page frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,20 @@
-
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
-   return <h1 className=' text-3xl font-bold underline '> Hello World   </h1>
+   return (
+      <>
+      <Routes>
+         <Route path='/' element={<HomePage/>} />
+         <Route path='/login' element={<LoginPage/>} />
+         <Route path='/signup' element={<SignUpPage/>} />
+      </Routes>
+      <Footer/>
+      </>
+   );
 
 }
 
